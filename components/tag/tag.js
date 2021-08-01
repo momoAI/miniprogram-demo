ce150d5e771157c9dbcec0c247c2d95c479115fe
luxu <luxu@fooww.com>
2021-08-01 22:12:47 +0800

    init

---
+++ components/tag/tag.js
@@ -0,0 +1,69 @@
+Component({
+  lifetimes: {
+    created: function() {
+      console.log('组件被创建')
+    },
+    attached: function() {
+      console.log('组件初始化完毕')
+      this.setData({
+        num1: 10,
+        num2: 22,
+      })
+      console.log('sum is ' + this.sum)
+    },
+    detached: function() {
+      console.log('组件离开节点或页面退出')
+    }
+  },
+  pageLifetimes: {
+    show: function() {
+      console.log('组件所在页面展示')
+    },
+    hide: function() {
+      
+    },
+    resize: function() {
+
+    }
+  },
+  options: {
+    pureDataPattern: /^_/ // 以_开头的为纯数据
+  },
+  observers: {
+    'num1,num2': function(n1, n2) {
+      this.sum = n1 + n2
+      // this.setData({
+      //   sum: n1 + n2
+      // })
+    }
+  },
+  properties: {
+    tagName: {
+      type: String,
+      value: "empty"
+    },
+    _pureProperty: {
+      type: String,
+      value: ''
+    }
+  },
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  data: {
+    innerData: "default text",
+    num1: 0,
+    num2: 0,
+    sum: 0,
+    _pureData: ''
+  },
+  methods: {
+    getInnerTag: function(param) {
+      console.log(param)
+    }
+  },
+  externalClasses: ['external'],
+  relations: {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,15 +4,20 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World First App',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
+    tagName: "",
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
   // 事件处理函数
-  bindViewTap() {
+  bindViewTap(event) {
+    this.setData({
+      "tagName": "hehe"
+    })
+    console.log(event)
     wx.navigateTo({
       url: '../logs/logs'
     })
